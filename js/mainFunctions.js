da8d74a17494e5d8d602dc33001984af01a01d26
Eila <eila.collings@gmail.com>
2021-07-22 23:37:59 -0400

    Switch to a basic blog template.

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -2,5 +2,4 @@ $(document).ready(() => {
     console.log('ready');
     $('.navbar_wraper').load('/components/navbar.html');    
     $('.footer_wraper').load('/components/footer.html');
-    
 });
